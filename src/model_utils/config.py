892ee8c1bf2bd8d44bbc3c7579cdbb56bc4a60a9
Capt_Hwi[1] <capthwi1.0@gmail.com>
2026-02-01 12:51:21 +0100

    Update config.py

--- src/model_utils/config.py
+++ src/model_utils/config.py
@@ -4,12 +4,7 @@ import argparse
 from pprint import pformat, pprint
 import yaml
 
-_config_path = "./ssd300_config.yaml"
-
 class Config:
-    """
-    Configuration namespace. Convert dictionary to members.
-    """
     def __init__(self, cfg_dict):
         for k, v in cfg_dict.items():
             if isinstance(v, (list, tuple)):
@@ -20,94 +15,50 @@ class Config:
     def __str__(self):
         return pformat(self.__dict__)
 
-    def __repr__(self):
-        return self.__str__()
-
-
-def parse_cli_to_yaml(parser, cfg, helper=None, choices=None, cfg_path="ssd300_config.yaml"):
-    """
-    Parse command line arguments to the configuration according to the default yaml.
-
-    Args:
-        parser: Parent parser.
-        cfg: Base configuration.
-        helper: Helper description.
-        cfg_path: Path to the default yaml config.
-    """
-    parser = argparse.ArgumentParser(description="[REPLACE THIS at config.py]",
-                                     parents=[parser])
+def parse_cli_to_yaml(parser, cfg, helper=None, choices=None, cfg_path="ssd300_config_gpu.yaml"):
+    parser = argparse.ArgumentParser(description="MindSpore SSD Configuration", parents=[parser])
     helper = {} if helper is None else helper
     choices = {} if choices is None else choices
     for item in cfg:
-        if not isinstance(cfg[item], list) and not isinstance(cfg[item], dict):
-            help_description = helper[item] if item in helper else "Please reference to {}".format(cfg_path)
+        if not isinstance(cfg[item], (list, dict)):
+            help_description = helper[item] if item in helper else f"Reference to {cfg_path}"
             choice = choices[item] if item in choices else None
             if isinstance(cfg[item], bool):
-                parser.add_argument("--" + item, type=ast.literal_eval, default=cfg[item], choices=choice,
-                                    help=help_description)
+                parser.add_argument("--" + item, type=ast.literal_eval, default=cfg[item], choices=choice, help=help_description)
             else:
-                parser.add_argument("--" + item, type=type(cfg[item]), default=cfg[item], choices=choice,
-                                    help=help_description)
-    args = parser.parse_args()
-    return args
-
+                parser.add_argument("--" + item, type=type(cfg[item]), default=cfg[item], choices=choice, help=help_description)
+    return parser.parse_args()
 
 def parse_yaml(yaml_path):
-    """
-    Parse the yaml config file.
-
-    Args:
-        yaml_path: Path to the yaml config.
-    """
+    if not os.path.exists(yaml_path):
+        raise FileNotFoundError(f"Configuration file not found at {yaml_path}")
     with open(yaml_path, 'r') as fin:
         try:
-            cfgs = yaml.load_all(fin.read(), Loader=yaml.FullLoader)
-            cfgs = [x for x in cfgs]
-            if len(cfgs) == 1:
-                cfg_helper = {}
-                cfg = cfgs[0]
-                cfg_choices = {}
-            elif len(cfgs) == 2:
-                cfg, cfg_helper = cfgs
-                cfg_choices = {}
-            elif len(cfgs) == 3:
-                cfg, cfg_helper, cfg_choices = cfgs
-            else:
-                raise ValueError("At most 3 docs (config description for help, choices) are supported in config yaml")
-            print(cfg_helper)
-        except:
-            raise ValueError("Failed to parse yaml")
-    return cfg, cfg_helper, cfg_choices
-
-
-def merge(args, cfg):
-    """
-    Merge the base config from yaml file and command line arguments.
-
-    Args:
-        args: Command line arguments.
-        cfg: Base configuration.
-    """
-    args_var = vars(args)
-    for item in args_var:
-        cfg[item] = args_var[item]
-    return cfg
-
+            cfgs = list(yaml.load_all(fin.read(), Loader=yaml.FullLoader))
+            if len(cfgs) == 1: return cfgs[0], {}, {}
+            elif len(cfgs) == 2: return cfgs[0], cfgs[1], {}
+            elif len(cfgs) == 3: return cfgs[0], cfgs[1], cfgs[2]
+        except Exception as e:
+            raise ValueError(f"Failed to parse yaml: {e}")
 
 def get_config():
-    """
-    Get Config according to the yaml file and cli arguments.
-    """
-    parser = argparse.ArgumentParser(description="default name", add_help=False)
+    parser = argparse.ArgumentParser(description="Default Config", add_help=False)
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    parser.add_argument("--config_path", type=str, default=os.path.join(current_dir, \
-        "../../config/ssd300_config.yaml"), help="Config file path")
+    # Improvised: Look for the YAML in the root directory first
+    root_dir = os.path.abspath(os.path.join(current_dir, "../../"))
+    default_yaml = os.path.join(root_dir, "ssd300_config_gpu.yaml")
+    
+    parser.add_argument("--config_path", type=str, default=default_yaml, help="Config file path")
     path_args, _ = parser.parse_known_args()
+    
     default, helper, choices = parse_yaml(path_args.config_path)
     args = parse_cli_to_yaml(parser=parser, cfg=default, helper=helper, choices=choices, cfg_path=path_args.config_path)
-    final_config = merge(args, default)
-    pprint(final_config)
-    print("Please check the above information for the configurations", flush=True)
-    return Config(final_config)
+    
+    # Merge CLI args into YAML defaults
+    for item in vars(args):
+        default[item] = getattr(args, item)
+        
+    pprint(default)
+    return Config(default)
 
 config = get_config()
